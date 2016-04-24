ea9c78bc4ab12e5fedfe80db02217db78fa47bdc
pandabrah <javi.den@gmail.com>
2016-04-23 22:40:44 -0700

    Rough code for grabbing objects
    Currently only detects when the "hand" is in range or not of an object. Problems with creating a joint and attaching the object at the point of contact.
    Will recode for vive controllers later

---
+++ Assets/Input/GrabandThrow.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabandThrow : MonoBehaviour {
+
+    FixedJoint grabJoint;
+    bool objectDetect;
+    private GameObject detectedObj;
+    private Transform attachPoint;
+
+    void Start()
+    {
+        attachPoint = GameObject.Find("AttachPoint").transform;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Object Touched");
+        objectDetect = true;
+        detectedObj = collider.gameObject;
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        objectDetect = false;
+        Debug.Log("No Object Detected");
+    }
+
+    void GrabDetectedObject(GameObject obj)
+    {
+        obj.transform.position = attachPoint.position;
+        grabJoint = obj.AddComponent<FixedJoint>();
+        grabJoint.connectedBody = attachPoint;
+
+    }
+
+    void FixedUpdate()
+    {
+        if (objectDetect == true && Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Grab Attempted");
+            GrabDetectedObject(detectedObj);
+        }
+    }
+}
