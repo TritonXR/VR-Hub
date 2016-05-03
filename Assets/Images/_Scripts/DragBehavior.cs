0ecb83995afa539496311018826ad101a3c5db5e
RussellXie7 <russellxie@outlook.com>
2016-05-03 00:00:13 -0700

    Just add some image

---
+++ Assets/Images/_Scripts/DragBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void OnDrag(){
+		transform.position = Input.mousePosition;
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
